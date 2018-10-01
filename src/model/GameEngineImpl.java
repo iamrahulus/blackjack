b2ea0e2071e86be36af158310b1c34468251bf07
Rahul Rahul <iamrahulus@gmail.com>
2018-10-01 07:48:26 -0400

    Modified Engine Impl

--- src/model/GameEngineImpl.java
+++ src/model/GameEngineImpl.java
@@ -10,10 +10,41 @@ import model.interfaces.PlayingCard;
 
 public class GameEngineImpl implements GameEngine {
 
+	GameEngineCallback gameEngineCallback = null;
+
 	@Override
 	public void dealPlayer(Player player, int delay) {
 		// TODO Auto-generated method stub
-		
+		/*
+		 * 1. deal a card to the player 2. call GameEngineCallback.nextCard(...)
+		 * 3. continue looping until the player busts (default value of
+		 * GameEngine.BUST_TOTAL=21) 4. GameEngineCallback.bustCard(...) 5. call
+		 * {@link GameEngineCallback#result(Player, int, GameEngine)} with final
+		 * result for player (the pre bust total) 6. update the player with
+		 * final result so it can be retrieved later
+		 */
+		PlayingCard card = null;
+		int pointsBeforeBust = 0;
+		while (true) {
+			card = dealACard();
+			setPlayerPointsBeforeDeal(player, card);
+			gameEngineCallback.nextCard(player, card, this);
+			if (player.getPoints() > GameEngine.BUST_LEVEL)
+				break;
+			pointsBeforeBust = player.getPoints();
+		}
+		this.gameEngineCallback.bustCard(player, card, this);
+		this.gameEngineCallback.result(player, pointsBeforeBust, this);
+		player.setPoints(pointsBeforeBust);
+	}
+
+	private void setPlayerPointsBeforeDeal(Player player, PlayingCard card) {
+		player.setPoints(player.getPoints() + card.getScore());
+	}
+
+	private PlayingCard dealACard() {
+		PlayingCard pc = new PlayingCardImpl();
+		return pc;
 	}
 
 	@Override
@@ -43,7 +74,7 @@ public class GameEngineImpl implements GameEngine {
 	@Override
 	public void addGameEngineCallback(GameEngineCallback gameEngineCallback) {
 		// TODO Auto-generated method stub
-
+		this.gameEngineCallback = gameEngineCallback;
 	}
 
 	@Override
